2b6e4e984a5567fd9adceebdeee351f824fc9cdc
binhex <megalith01@gmail.com>
2017-12-28 12:55:46 +0000

    create dir to store config files in

--- run/nobody/start.sh
+++ run/nobody/start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# create dir to store config files in
+mkdir -p /config/nzbhydra
+
 # run app
 /usr/bin/python2 /usr/lib/nzbhydra/nzbhydra.py --nobrowser --config /config/nzbhydra/settings.cfg --database /config/nzbhydra/nzbhydra.db
